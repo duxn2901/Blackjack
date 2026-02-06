ef8f4370622237039dd27bbcaef540d495905ba9
duxn <vdb2901k55@gmail.com>
2026-02-06 22:54:28 +0100

    Refactor Card class: add attributes and constructor for rank and suit, add class documentation

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,22 @@
-public class Card {
+/* 
+    attributes
+        String rank
+        String suit
+        int value
+        boolean isAce
 
+    toString()
+
+*/
+
+public class Card {
+    private String rank;
+    private String suit;
+    private int value;
+    private boolean isAce = false;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
 }

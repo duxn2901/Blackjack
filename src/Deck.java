94899153b209156cd0b4b19336f9b43dc7f30129
duxn <vdb2901k55@gmail.com>
2026-02-10 00:01:14 +0100

    Add class Deck constructor, methods printDeck() and generate()

--- src/Deck.java
+++ src/Deck.java
@@ -1,7 +1,11 @@
+
+import java.util.ArrayDeque;
+
 /*
     Deck : Deque Stack
         generate()
             nested loop for each enums Rank, Suit
+        printDeck()
         shuffle()
         addToTop(Card)
             deque.push()
@@ -13,5 +17,23 @@
 */
 
 public class Deck {
+    ArrayDeque<Card> deck;
 
+    public Deck() {
+        deck = new ArrayDeque<>();
+    }
+
+    public void printDeck() {
+        for (Card card : deck) System.out.print(card + "\t");
+    }
+
+    public void generate(int numberOfDecks) {
+        for (int i = 0; i < numberOfDecks; i++) {
+            for (Rank rank : Rank.values()) {
+                for (Suit suit : Suit.values()) {
+                    deck.push(new Card(rank, suit));
+                }
+            }
+        }
+    }
 }

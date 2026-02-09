41806b1020816e454117731c40893cddc9f3edbf
duxn <vdb2901k55@gmail.com>
2026-02-09 23:43:30 +0100

    Add class Deck skeleton

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,17 @@
+/*
+    Deck : Deque Stack
+        generate()
+            nested loop for each enums Rank, Suit
+        shuffle()
+        addToTop(Card)
+            deque.push()
+        dealCard(Hand)
+            deque.pop()
+        getTopCard()
+            deque.peek()
+            
+*/
+
 public class Deck {
 
 }

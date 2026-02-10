6ed75f2a51110440660176954a3ef201d5103658
duxn <vdb2901k55@gmail.com>
2026-02-10 18:38:12 +0100

    Add gameflow

--- src/App.java
+++ src/App.java
@@ -1,4 +1,42 @@
+/*
+    Gameflow:
+        Start
+        number of decks -> generate deck, shuffle
+        number of players
+        starting money
 
+        Game loop
+            bet amount
+            deal cards to players and dealer
+            calc hand value
+            check for blackjacks -> return wins
+            for each player
+                while hand active
+                    ask choice
+                    proceed with choice
+                        if hit -> deal 1 card
+                        if stand -> update state = STOOD
+                        if hand length == 2
+                            allow double
+                                deal 1 card, handle bet amount 1 more time, update state = DOUBLED 
+                        if hand length == 2 and card 0 == card 1
+                            allow split
+                                init new hand, pop 1 card out for the new hand
+                                deal new cards to 2 hands
+                                continue
+
+                    calc hand value
+                    decide hand state
+                        if bust
+                            update state to BUSTED
+
+            dealer flips, calc hand value, draws until at least 17 
+                if bust -> return wins to all STOOD, DOUBLED hands
+                else -> compare scores with all STOOD, DOUBLED hands -> return win/lost
+            reset all players' hands
+            reset dealer's hand
+            return balance
+*/
 
 public class App {
     public static void main(String[] args) throws Exception {

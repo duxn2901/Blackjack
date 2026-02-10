98c07ce4713d42093516189ef29b13ae1e2cf849
duxn <vdb2901k55@gmail.com>
2026-02-10 15:35:31 +0700

    Class Hand: add attribute, constructor, methods .getHand()

--- src/Hand.java
+++ src/Hand.java
@@ -1,3 +1,17 @@
+
+import java.util.ArrayList;
+
 public class Hand {
+    private final ArrayList<Card> hand;
+
+    public Hand() {
+        hand = new ArrayList<>();
+    }
+
+    public String getHand() {
+        String handString = "";
+        for (Card card : hand) handString += card + "\t";
+        return handString;
+    }
 
 }

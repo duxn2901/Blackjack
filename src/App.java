82e131b751411a0b19e108188c46fb97d2ea7651
duxn <vdb2901k55@gmail.com>
2026-02-10 10:44:18 +0100

    Add tests

--- src/App.java
+++ src/App.java
@@ -2,15 +2,21 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Card newCard = new Card(Rank.QUEEN, Suit.HEARTS);
+        // Card newCard = new Card(Rank.QUEEN, Suit.HEARTS);
         // System.out.println(newCard);
         // System.out.println(newCard.getValue());
         // System.out.println(newCard.getisAce());
         Deck deck = new Deck();
-        deck.generate(2);
-        System.out.println(deck.getDeck());
-        System.out.println(deck.getTopCard());
-        deck.addToTop(newCard);
-        System.out.println(deck.getTopCard());
+        deck.generate(1);
+        System.out.println(deck);
+        // System.out.println(deck.getTopCard());
+        // deck.addToTop(newCard);
+        // System.out.println(deck.getTopCard());
+        Hand hand = new Hand();
+        hand.addCardToHand(deck.dealTopCard());
+        hand.addCardToHand(deck.dealTopCard());
+        System.out.println(hand);
+        hand.updateHandValue(hand.calculateHandValue());
+        System.out.println(hand.getHandValue());
     }
 }

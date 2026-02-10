e32abd294851cf2ec5f054bb7e1b956eb876a665
duxn <vdb2901k55@gmail.com>
2026-02-10 01:03:52 +0100

    Add testcases in App,java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Card newCard = new Card(Rank.QUEEN, Suit.HEARTS);
+        // System.out.println(newCard);
+        // System.out.println(newCard.getValue());
+        // System.out.println(newCard.getisAce());
+        Deck deck = new Deck();
+        deck.generate(2);
+        deck.printDeck();
+        System.out.println(deck.getTopCard());
+        deck.addToTop(newCard);
+        System.out.println(deck.getTopCard());
     }
 }

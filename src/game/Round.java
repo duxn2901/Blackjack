cb5a619986856f45c0d61b54075b3009aee344d0
duxn <vdb2901k55@gmail.com>
2026-05-09 18:56:46 +0200

    Class Round: init

---
+++ src/game/Round.java
@@ -0,0 +1,20 @@
+package game;
+
+import java.util.ArrayList;
+import model.*;
+
+public class Round {
+    Deck deck;
+    ArrayList<Player> players;
+    Dealer dealer;
+
+    public Round(Deck deck, ArrayList<Player> players, Dealer dealer) {
+        this.deck = deck;
+        this.players = players;
+        this.dealer = dealer;
+    }
+
+    
+
+
+}

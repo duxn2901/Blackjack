4b16240828e00cd7ff5c49a2797cc42467c4efc9
duxn <vdb2901k55@gmail.com>
2026-05-09 19:36:29 +0200

    Class Main: main method to initialize game

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,29 @@
 package app;
 
+import game.Game;
+import java.util.ArrayList;
+import model.*;
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("123");
+        Deck deck = new Deck();
+        deck.generate(1); //changeable
+        deck.shuffle();
+
+        Dealer dealer = new Dealer();
+
+        ArrayList<Player> players = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) { //changeable
+            players.add(new Player("Player "+i)); //changeable
+        }
+
+        for (Player p : players) {
+            for (int i = 1; i <= 2; i++) {
+                p.hands.add(new Hand(i));
+            }
+        }
+        
+        Game game = new Game(deck, players, dealer);
+        game.start();
     }
 }

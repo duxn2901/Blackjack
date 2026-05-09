3307b694a296e6aa183eebb4a1382d0a07a94be5
duxn <vdb2901k55@gmail.com>
2026-05-09 19:39:17 +0200

    Round class: round logic, new method dealInitial() and tests

--- src/game/Round.java
+++ src/game/Round.java
@@ -14,7 +14,34 @@ public class Round {
         this.dealer = dealer;
     }
 
-    
+    public void start() {
+        dealInitial();  //deal 2 at the start of round to each hand for each player including dealer
+        // handlePlayerTurn(); //handle turn logic for each player
+        // handleDealerTurn(); //handle turn logic of dealer
+        // checkResults(); //checking result after dealer turn
+        System.out.println("Dealer's hand:"); //TODO delete
+        System.out.println(dealer.hand.showHand());
+        System.out.println();
+        for (Player p : players) {
+            for (Hand h : p.hands) {
+                System.out.println(p + " " + h);
+                System.out.println(h.showHand());
+            }
+        }
+    }
+
+    public void dealInitial() {
+        for (Player p : players) {
+            for (Hand h : p.hands) {
+                System.out.println("Dealing cards to " +h);
+                h.addCardToHand(deck.dealCard());
+                h.addCardToHand(deck.dealCard());
+            }
+        }
+        System.out.println("Dealing cards to dealer");
+        dealer.hand.addCardToHand(deck.dealCard());
+        dealer.hand.addCardToHand(deck.dealCard());
+    }
 
 
 }
